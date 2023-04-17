dafa46b971f0a031b07682dd6980ca6a4f0e9674
gabbestartklar <gabriel.yanezcarrasco@skola.taby.se>
2023-04-17 08:52:02 +0200

    commit

---
+++ GTA.js
@@ -0,0 +1,8 @@
+
+window.onload = function() {
+    var canvas = document.getElementById("MAP");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById(HUS);
+    ctx.drawImage(img, 10, 10);
+
+  };
